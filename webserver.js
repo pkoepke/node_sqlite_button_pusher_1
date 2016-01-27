f63e78a35c00f30d664eaabc683044084d0ea383
Paul Koepke <paul.koepke@gmail.com>
2016-01-27 11:21:40 -0500

    allowing web server to respond to any IPv4 or IPv6 address

--- webserver.js
+++ webserver.js
@@ -41,8 +41,8 @@ function handleRequest(request, response) {
 /* http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.end('Hello World\n');
-}).listen(listenPort, '127.0.0.1'); */
+}).listen(listenPort); */
 
 // Start the web server, direct all requests to handleRequest()
-http.createServer(handleRequest).listen(listenPort, '127.0.0.1');
+http.createServer(handleRequest).listen(listenPort);
 console.log('Server running at http://127.0.0.1:' + listenPort + '/');
