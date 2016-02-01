e40901aafdcc432de9ebfcbfc03f86e19dfb5e5f
Paul Koepke <paul.koepke@gmail.com>
2016-02-01 13:49:19 -0500

    Changed responses to reverse chronological order. Also found that response list is still returning before DB write is finished.

--- read_from_button_db.js
+++ read_from_button_db.js
@@ -4,11 +4,12 @@ var db = new sqlite3.Database('button_db.db');
 function readFromButtonDb(responseBody, callback) {
   // default to an empty function if no callback is specified
   if (typeof callback === 'undefined') { callback = function() {}; }
-  var returnRows = "";
+  var returnRowsReverseOrder = "";
   db.all("SELECT ROWID, datetime, clientIp from button_info", function(err,rows) {
     rows.forEach(function(currentRow) {
-      responseBody += "Entry " + currentRow.rowid + ": <span class=\"buttonPushTime\">" + currentRow.datetime + "</span> | Client IP address: " + currentRow.clientIp + "<br />\n";
+      returnRowsReverseOrder = "Entry " + currentRow.rowid + ": <span class=\"buttonPushTime\">" + currentRow.datetime + "</span> | Client IP address: " + currentRow.clientIp + "<br />\n" + returnRowsReverseOrder;
     });
+    responseBody += returnRowsReverseOrder;
     callback(responseBody);
   });
 }
