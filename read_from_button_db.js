98d66a72049676998d8bbc0a245f4247d83fd9d4
Paul Koepke <paul.koepke@gmail.com>
2016-02-01 21:35:18 -0500

    Turned entries into a HTML table.

--- read_from_button_db.js
+++ read_from_button_db.js
@@ -7,8 +7,10 @@ function readFromButtonDb(responseBody, callback) {
   var returnRowsReverseOrder = "";
   db.all("SELECT ROWID, datetime, clientIp from button_info", function(err,rows) {
     rows.forEach(function(currentRow) {
-      returnRowsReverseOrder = "Entry " + currentRow.rowid + ": <span class=\"buttonPushTime\">" + currentRow.datetime + "</span> | Client IP address: " + currentRow.clientIp + "<br />\n" + returnRowsReverseOrder;
+      returnRowsReverseOrder = "<tr><td>Entry " + currentRow.rowid + "</td><td><span class=\"buttonPushTime\">" + currentRow.datetime + "</span></td><td>Client IP address: " + currentRow.clientIp + "</td></tr>\n" + returnRowsReverseOrder;
     });
+    returnRowsReverseOrder = "<table>\n" + returnRowsReverseOrder;
+    returnRowsReverseOrder += "</table>";
     responseBody += returnRowsReverseOrder;
     callback(responseBody);
   });
