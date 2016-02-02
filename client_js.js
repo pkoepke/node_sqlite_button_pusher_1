05e9ca5f43af0d37a6a2c8e5772a1e49330f502c
Paul Koepke <paul.koepke@gmail.com>
2016-02-01 21:46:40 -0500

    Removing final optional line of datetime conversion

--- client_js.js
+++ client_js.js
@@ -23,7 +23,7 @@ function convertTimeToClientTimezone(dateTimeToConvert) {
   dateTimeToConvert = new Date(dateTimeToConvert); // create new Date object based on dateTime
   dateTimeToConvert = dateTimeToConvert.getTime(); // turn Date object into milliseconds since 1970/01/01. Removes time zones entirely as miliseconds are universal. Tested same time in different time zones and the date > miliseconds > date conversion works correctly.
   dateTimeToConvert = new Date(dateTimeToConvert); // create a new Date object using miliseconds. Will be in client's time zone.
-  dateTimeToConvert = dateTimeToConvert.toISOString().slice(0,10) + " " + dateTimeToConvert.toISOString().slice(11,18) + " (" + dateTimeToConvert.toString().slice(-4,-1) + ")"; // Shorten the length of the datetime while keeping the timezone indicator. Everything will still work if this line is commented out.
+  // dateTimeToConvert = dateTimeToConvert.toISOString().slice(0,10) + " " + dateTimeToConvert.toISOString().slice(11,18) + " (" + dateTimeToConvert.toString().slice(-4,-1) + ")"; // Shorten the length of the datetime while keeping the timezone indicator. Everything will still work if this line is commented out.
   return dateTimeToConvert;
 }
 
